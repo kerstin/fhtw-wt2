4964f84b63b684448155a810a4adf5d397e9dc00
Kerstin Kollmann <code@kerstinkollmann.com>
2014-03-24 15:40:33 +0100

    da main fileee

---
+++ index.php
@@ -0,0 +1,54 @@
+<?
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="css/webtop.css" rel="stylesheet" type="text/css">
+        <script src="js/webtop.js">
+        </script>
+
+        <script>
+
+            window.onresize = bgresize
+
+            function bgresize () {
+              w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+              h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+              document.getElementsByTagName('body')[0].style.backgroundSize = '' +w+ 'px ' +h+ 'px'
+              taskbar(w,h)
+            }
+
+            function taskbar(width,height) {
+              somesize = Math.random()*100
+              // document.getElementById('taskbar').style.backgroundColor = '#5623f5'
+              // document.getElementById('taskbar').style.width = width + 'px'
+              // document.getElementById('taskbar').style.height = height-300 + 'px'
+            }
+
+        </script>
+    </head>
+
+    <body>
+        hello
+
+        <div id="taskbar">
+            <div id="startbutton">
+            </div>
+          hello
+        </div>
+
+
+        <div id="icon1" class="icon">&nbsp;
+        </div>
+        <div id="icon2" class="icon">&nbsp;
+        </div>
+        <div id="icon3" class="icon">&nbsp;
+        </div>
+
+
+
+        <!-- <img src="img/trollface-bg.jpg"> -->
+
+
+    </body>
+</html>
