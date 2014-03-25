a10ef6316d0b23c6d798605c38f9f124b17baa69
Kerstin Kollmann <code@kerstinkollmann.com>
2014-03-25 14:20:49 +0100

    separate desktop so it only gets displayed on successful login

---
+++ desktop.php
@@ -0,0 +1,46 @@
+<?
+?>
+        <div id="desktopcontainer" ondrop="drop(event)">
+            <div class="icon" id="icon1">
+                <img src="img/grumpycat-75px.png">
+            </div>
+            <div class="icon" id="icon2" draggable="true">
+                <img src="img/doge-75px.png">
+            </div>
+            <div class="icon" id="icon3" draggable="true">
+                <img src="img/revengebaby-75px.png">
+            </div>
+        </div>
+        <div id="maincontainer">
+            <div id="taskbar_container">
+                <div id="taskbar" class="menus">
+                    <div id="startbutton" class="icon">
+                        <img src="img/trolllaugh-30px.png">
+                    </div>
+                </div>
+            </div>
+            <div id="startmenu" class="menus hidden">
+                <div class="startmenu_item">
+                    Item #1
+                </div>
+                <div class="startmenu_item">
+                    Item #2 has a longer name
+                </div>
+                <div class="startmenu_item">
+                    Item #3
+                </div>
+                <div class="startmenu_item">
+                    &nbsp;
+                </div>
+            </div>
+            <div id="phpinfo_container" class="hidden">
+                <div class="topbar menus">
+                    <div id="closebutton" class="closebutton menus">
+                        x
+                    </div>
+                </div>
+                <div class="htmlobject">
+                    <object class="htmlobject" type="text/html" data="phpinfo.php">
+                </div>
+            </div>
+        </div>
