acc1d05f07a1f0826a5bbe02b54886eed89884f9
Kerstin Kollmann <code@kerstinkollmann.com>
2014-04-10 00:10:14 +0200

    added basic index file

---
+++ index.php
@@ -0,0 +1,25 @@
+<?
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Webtop</title>
+
+        <!-- <link href="css/webtop.css" rel="stylesheet" type="text/css"> -->
+        <!-- <script src="js/webtop.js"></script> -->
+
+         <style>
+        </style>
+
+        <script>
+        </script>
+    </head>
+
+    <body>
+
+        <div>
+        Hello!
+        </div>
+    </body>
+</html>
