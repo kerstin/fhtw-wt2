51909766da38ffa57b5baba1ad877554ddc58bd8
Kerstin Kollmann <code@kerstinkollmann.com>
2014-04-13 21:54:24 +0200

    script to set original positions of elements

---
+++ positions.php
@@ -0,0 +1,11 @@
+<?
+/*  define original positions of elements
+    currently still manually written */
+
+(!$_SESSION['paX']) ? ($_SESSION['paX'] = 450) : '';
+(!$_SESSION['paY']) ? ($_SESSION['paY'] = 250) : '';
+(!$_SESSION['kaX']) ? ($_SESSION['kaX'] = 250) : '';
+(!$_SESSION['kaY']) ? ($_SESSION['kaY'] = 150) : '';
+(!$_SESSION['phpaX']) ? ($_SESSION['phpaX'] = 300) : '';
+(!$_SESSION['phpaY']) ? ($_SESSION['phpaY'] = 50) : '';
+?>
