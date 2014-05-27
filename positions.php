c1fec484004d2de042cacc2e834e17ae943b6e6e
Kerstin Kollmann <code@kerstinkollmann.com>
2014-05-27 16:20:22 +0200

    add initial positions for rss feed window

--- positions.php
+++ positions.php
@@ -3,6 +3,8 @@
     (currently still manually written) */
 $init_phpappX = 300;
 $init_phpappY = 50;
+$init_rssappX = 500;
+$init_rssappY = 300;
 $init_kontoappX = 250;
 $init_kontoappY = 150;
 $init_photoappX = 450;
@@ -11,6 +13,9 @@ $init_photoappY = 250;
 // php info app
 (!$_SESSION['phpappX']) ? ($_SESSION['phpappX'] = $init_phpappX) : '';
 (!$_SESSION['phpappY']) ? ($_SESSION['phpappY'] = $init_phpappY) : '';
+// rss app
+(!$_SESSION['rssappX']) ? ($_SESSION['rssappX'] = $init_rssappX) : '';
+(!$_SESSION['rssappY']) ? ($_SESSION['rssappY'] = $init_rssappY) : '';
 // konto app
 (!$_SESSION['kontoappX']) ? ($_SESSION['kontoappX'] = $init_kontoappX) : '';
 (!$_SESSION['kontoappY']) ? ($_SESSION['kontoappY'] = $init_kontoappY) : '';
